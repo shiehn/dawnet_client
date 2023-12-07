65d65c3d92260e7c1391defa377ad93aa86b2dea
Steve Hiehn <stevehiehn@Steves-Mac-mini.hitronhub.home>
2023-12-06 16:38:55 -0800

    bump to v0016; fix import in README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dawnet-client',
-    version='0.0.13',
+    version='0.0.16',
     packages=find_packages(),
     install_requires=[
         'websockets',
