8f3d0f5340cef9cfa91716b8617b73f2d1a76f1a
Steve Hiehn <stevehiehn@Steves-Mac-mini.hitronhub.home>
2024-01-16 13:19:56 -0800

    updated IP to the public server

--- dawnet_client/config.py
+++ dawnet_client/config.py
@@ -2,6 +2,6 @@ import os
 
 # --------- PRODUCTION SETTINGS ----------------
 SOCKET_IP = os.getenv('DN_CLIENT_SOCKET_IP', '35.223.141.253')
-API_BASE_URL = os.getenv('DN_CLIENT_API_BASE_URL', 'https://signalsandsorcery.ai')
+API_BASE_URL = os.getenv('DN_CLIENT_API_BASE_URL', '35.223.141.253:')
 SENTRY_API_KEY = os.getenv('DN_CLIENT_SENTRY_API_KEY', "https://dbbf2855a707e0448957ad77111449c3@o4506379662131200.ingest.sentry.io/4506379670847488")
 SOCKET_PORT = os.getenv('DN_CLIENT_SOCKET_PORT', '8765')
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if not is_ffmpeg_installed():
 
 setup(
     name='dawnet-client',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'websockets',
